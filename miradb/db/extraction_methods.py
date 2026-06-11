8a5bbded4b8da165fae3b9a760cec5aa7123c4b2
kkaris <karis.klas@gmail.com>
2026-06-11 10:42:44 -0700

    Add label mapping

--- miradb/db/extraction_methods.py
+++ miradb/db/extraction_methods.py
@@ -7,3 +7,6 @@ with EXTRACTION_METHODS_PATH.open("r") as f:
     json_data = json.load(f)
     EXTRACTION_METHODS_INFO = json_data["extraction_methods"]
     EXTRACTION_METHODS_PRIORITY = json_data["extraction_method_priority"]
+    EXTRACTION_METHOD_LABELS = {
+        m["extraction_method"]: m["label"] for m in EXTRACTION_METHODS_INFO
+    }
